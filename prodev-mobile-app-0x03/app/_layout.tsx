5441c399bb4ba9e36a96635a2437b07fd3bb5beb
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-08-31 18:53:56 +0200

    stack options

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -2,8 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
+    <Stack screenOptions={{
         headerShown: false,
       }}
     />
